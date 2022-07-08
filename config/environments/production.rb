ec20fe53529d05b68bec3a0d9672422d2ca3d226
nashikotokyo <natsumi.creo@gmail.com>
2022-07-08 13:38:19 +0900

    [Add]アプリ名をMy_Guilty_Petに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "criminal_pets_production"
+  # config.active_job.queue_name_prefix = "my_guilty_pet_production"
 
   config.action_mailer.perform_caching = false
 
