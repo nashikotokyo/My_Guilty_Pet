c2f8a91ab3d95ec0f6b2e1c6b98648ef424deb7f
nashikotokyo <natsumi.creo@gmail.com>
2022-08-01 00:03:00 +0900

    [Add]利用規約とプライバシーポリシーの作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,7 @@
 class StaticPagesController < ApplicationController
   def home; end
+
+  def terms; end
+
+  def privacy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get 'terms', to: 'static_pages#terms'
+  get 'privacy', to: 'static_pages#privacy'
 end
