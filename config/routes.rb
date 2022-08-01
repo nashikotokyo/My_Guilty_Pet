d7a7cb65e42d8ae298761431738e4667c455d190
nashikotokyo <natsumi.creo@gmail.com>
2022-08-01 17:03:33 +0900

    [Add]お問い合わせページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'terms', to: 'static_pages#terms'
   get 'privacy', to: 'static_pages#privacy'
+  get 'contact', to: 'static_pages#contact'
 end
