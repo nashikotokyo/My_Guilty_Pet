8461892c707054cdeba8b013563df55c4a3caed1
nashikotokyo <natsumi.creo@gmail.com>
2022-08-05 15:37:39 +0900

    [Add]sorceryでuserモデルとテーブルを作成

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  username   :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_username  (username) UNIQUE
+#
+class User < ApplicationRecord
+  authenticates_with_sorcery!
+
+  validates :username, uniqueness: true, presence: true
+end
