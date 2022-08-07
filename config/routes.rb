23c4689aea87cfeca6557b4f2f6fc94e684045f4
nashikotokyo <natsumi.creo@gmail.com>
2022-08-07 21:34:58 +0900

    [Add]oauthsコントローラのためのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   get 'terms', to: 'static_pages#terms'
   get 'privacy', to: 'static_pages#privacy'
   get 'contact', to: 'static_pages#contact'
+
+  post 'oauth/:provider/callback', to: 'oauths#callback'
+  get 'oauth/:provider/callback', to: 'oauths#callback' # for use with Github, Facebook
+  get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
 end
