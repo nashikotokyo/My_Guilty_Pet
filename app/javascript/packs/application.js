8b4732a609f275593de7b071d68e0bde00d014b4
nashikotokyo <natsumi.creo@gmail.com>
2022-09-15 18:02:00 +0900

    [Add]jquryをwebpackerで導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
 
 const images = require.context("../images", true);
 const imagePath = name => images(name, true);
