5fd216e2dc18b258bb2a0daa9353e9274a2404e3
nashikotokyo <natsumi.creo@gmail.com>
2022-10-04 18:44:27 +0900

    [Add]テスト用のダミーログインを作成

---
+++ app/controllers/development/sessions_controller.rb
@@ -0,0 +1,7 @@
+class Development::SessionsController < ApplicationController
+  def login_as
+    user = User.find(params[:user_id])
+    auto_login(user)
+    redirect_to posts_path, success: "ログインしました"
+  end
+end
