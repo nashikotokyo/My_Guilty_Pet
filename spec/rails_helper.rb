670f77df3659f8eedebcd8b28fd384a513cec9b5
nashikotokyo <natsumi.creo@gmail.com>
2022-10-04 10:57:05 +0900

    [Add]capybaraやシステムスペックの初期設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'capybara/rspec'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -20,7 +22,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -64,4 +66,10 @@ RSpec.configure do |config|
 
   #rspecのテストコード中でFactory_botのメソッド使用の際、クラス名の指定を省略できる
   config.include FactoryBot::Syntax::Methods
+
+   # before(:each) => type: :system（つまりシステムスペック）で書かれたテストの各exampleの前に毎回実行される処理
+   config.before(:each, type: :system) do
+    # driven_by(ドライバの種類, using: 使うブラウザ, screen_size: [XXXX, XXXX], options: オプション={})
+    driven_by :selenium, using: :headless_chrome, screen_size: [1920, 1080]
+  end
 end
